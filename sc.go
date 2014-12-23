d7bad155979d89bbb91be4b52e670c003667f8dd
David R. Jenni <david.r.jenni@gmail.com>
2014-12-23 20:28:50 +0100

    Add main package with documentation.

---
+++ sc.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2014 David R. Jenni. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Sc is a simple toy compiler. It compiles a simple subset of the Go programming language.
+
+Installation:
+	% go get github.com/davidrjenni/sc
+
+Example usage:
+	% sc examples/gcf.sc -o gcf
+	% ./gcf
+*/
+package main
